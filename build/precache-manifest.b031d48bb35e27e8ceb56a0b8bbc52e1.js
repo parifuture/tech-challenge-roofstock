9b4e3166cb00680d88449c3900bb7dd4fb75469a
Parikshit Tiwari <pari.future@gmail.com>
2020-02-12 12:29:38 -0800

    Adding ghpages module

---
+++ build/precache-manifest.b031d48bb35e27e8ceb56a0b8bbc52e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a5dcd4e6a846b7a47545273924af678",
+    "url": "/tech-challenge-roofstock/index.html"
+  },
+  {
+    "revision": "1b92907d10569e6c4d19",
+    "url": "/tech-challenge-roofstock/static/css/main.ec830657.chunk.css"
+  },
+  {
+    "revision": "68037650fec4e606abb0",
+    "url": "/tech-challenge-roofstock/static/js/2.2dd8174f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tech-challenge-roofstock/static/js/2.2dd8174f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b92907d10569e6c4d19",
+    "url": "/tech-challenge-roofstock/static/js/main.77d81941.chunk.js"
+  },
+  {
+    "revision": "7395adcefc54c3116949",
+    "url": "/tech-challenge-roofstock/static/js/runtime-main.9509e4ea.js"
+  }
+]);
